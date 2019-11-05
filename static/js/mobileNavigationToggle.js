0a952486640231af19115903506c5fe0e6928380
Vadim Suhovskiy <vadim.suhovskiy@gmail.com>
2019-11-05 23:08:20 +0200

    pretify home page, some fixes for home page styles on mobile

--- static/js/mobileNavigationToggle.js
+++ static/js/mobileNavigationToggle.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function () {
+
+    
+
+    if (window.matchMedia("(max-width: 576px)").matches) {
+
+        var videoBlock = $('.video-js')            
+        
+        videoBlock.css("height", (window.innerWidth - 20) / 16 * 9)
+        
+    }
+
     $('.js-navigation-toggle').on('click', function(){
         $('.navigation').addClass('active');
     })
