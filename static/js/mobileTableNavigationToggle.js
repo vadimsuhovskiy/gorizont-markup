fdd2c20c9d7b1c3d5b382387433db5861de48b06
Vadim Suhovskiy <vadim.suhovskiy@gmail.com>
2019-11-22 19:27:28 +0200

    fixes for small pages

--- static/js/mobileTableNavigationToggle.js
+++ static/js/mobileTableNavigationToggle.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function () {
 
     if (window.matchMedia("(max-width: 576px)").matches) {
 
-        $('.products_tabs.household_filters h1').on('click', function(){
-            $('.products_tabs.household_filters').toggleClass('toggled')
+        $('.products_tabs h1').on('click', function(){
+            $('.products_tabs').toggleClass('toggled')
         })
         
     }
