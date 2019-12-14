6ae23d67122eeb71fbeaa4cdef4f184814fb0a65
Vadim Suhovskiy <vadim.suhovskiy@gmail.com>
2019-12-14 17:48:08 +0200

    update navigation menu styles

--- static/js/mobileNavigationToggle.js
+++ static/js/mobileNavigationToggle.js
@@ -11,9 +11,6 @@ jQuery(document).ready(function () {
     }
 
     $('.js-navigation-toggle').on('click', function(){
-        $('.navigation').addClass('active');
-    })
-    $('.js-navigation-hide').on('click', function(){
-        $('.navigation').removeClass('active');
+        $('.navigation').toggleClass('active');
     })
 });
